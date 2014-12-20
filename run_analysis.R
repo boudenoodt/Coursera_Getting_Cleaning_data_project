f73d2dc728c3a60211544f75c6e2c4486b1492d8
boudenoodt <bou@belgocontrol.be>
2014-12-20 21:38:04 +0100

    new R file added

--- run_analysis.R
+++ run_analysis.R
@@ -87,24 +87,25 @@ x_train<-cbind(sub_train,x_train)
 x_train_test<-rbind(x_train,x_test)
 
 ## select mean / Mean / std / subject_id / activity_id variables only
-## keep the variables/column related to "angle" (7 pieces)
-## eliminate the variables with mean in the middle of the name ("...meanFreq...")
+## remove the variables/column related to "angle" and "meanFreq"
 ## and keep only those variabled/columns by subsetting  and "select"
  vars_to_retain<-corrected_features_names[grepl("mean$|Mean$|std$|mean.|Mean.|std.",corrected_features_names)] 
 ## eliminate the variables/column with "meanFreq" in the name as this are probably not real means (I guess)
- vars_to_retain<-vars_to_retain[!grepl("meanFreq",vars_to_retain)] 
- vars_to_retain<-c("subject_id","activity_id",as.character(vars_to_retain))
- x_train_test_mean_std_only<-subset(x_train_test,select=vars_to_retain)
+vars_to_retain<-vars_to_retain[!grepl("meanFreq",vars_to_retain)] 
+## eliminate the variables/column with "angle" in the name as this are probably not real means but moving averages(I guess)
+vars_to_retain<-vars_to_retain[!grepl("^angle",vars_to_retain)] 
+vars_to_retain<-c("subject_id","activity_id",as.character(vars_to_retain))
+x_train_test_mean_std_only<-subset(x_train_test,select=vars_to_retain)
 
 ## Replace the "activity_id" by the activity labels and change the column order
 ## ("LAYING","SITTING","STANDING","WALKING","WALKING_DOWNSTAIRS","WALKING_UPSTAIRS") using the merge fuction
 x_train_test_mean_std_only<-merge(x=x_train_test_mean_std_only,y=activity_labels,by.x="activity_id",by.y="activity_id")
-x_train_test_mean_std_only<-subset(x_train_test_mean_std_only,select=c(2,76,3:75))
+x_train_test_mean_std_only<-subset(x_train_test_mean_std_only,select=c(2,69,3:68))
 
 ## calculate the mean for every combination of people/activity/variable using ddply from the "plyr" package
 x_train_test_mean_std_only<-ddply(x_train_test_mean_std_only,as.quoted(c("subject_id","activity")),colwise(mean))
 ## change the varable/column names to start with "mean.of." since now it is the mean of "a mean" or a "std"
-colnames(x_train_test_mean_std_only)<-c("subject_id","activity",paste0("mean.of.",colnames(x_train_test_mean_std_only[3:75])))
+colnames(x_train_test_mean_std_only)<-c("subject_id","activity",paste0("mean.of.",colnames(x_train_test_mean_std_only[3:68])))
 
 ## script output
 x_train_test_mean_std_only
